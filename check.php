47bb5cbfc90c3fc7268a29dc06022654b6a859c3
etpdgzcvb <42503053+etpdgzcvb@users.noreply.github.com>
2019-04-21 20:11:30 -0400

    Delete check.php

--- check.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Define session data file based on GET params passed through from index.html
-$file = "session/" . $_GET['s'];
-//Echo the contents of the session-appropriate file
-echo file_get_contents($file);
-?>
